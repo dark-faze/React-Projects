15ce7c9f41d98ce7c2c26008e5c808087e7cab29
dark-faze <68679017+dark-faze@users.noreply.github.com>
2021-08-06 17:36:12 +0530

    Added Header Component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
-import './App.css'
+import React from 'react';
+import Header from './Header';
+import './App.css';
+
 
  function App() {
     return (
         <div>
-            <h1>Yo this is lit</h1>
+            <Header/>
         </div>
-    )
+    );
 }
 
 export default App;
